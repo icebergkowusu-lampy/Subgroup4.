3bf6eb19ee97f6394471542e3b1b6c99ab8ca569
Lampy <icebergkowusu@gmail.com>
2026-08-23 08:19:48 +0000

    Delete MathVerse-SHS-Functions-Mobile-Responsive-Final directory

--- MathVerse-SHS-Functions-Mobile-Responsive-Final/js/mobile-responsive.js
+++
@@ -1,1 +0,0 @@
-(function(){function r(){document.querySelectorAll("canvas").forEach(function(c){if(c.parentElement&&c.parentElement.clientWidth){c.style.maxWidth="100%";}})}window.addEventListener("resize",r,{passive:true});window.addEventListener("orientationchange",r,{passive:true});document.addEventListener("DOMContentLoaded",r);})();
